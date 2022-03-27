318e6e487e58d2a624820bd769369ad700774808
Amal Thundiyil <amalthundiyil0@gmail.com>
2022-03-27 17:17:34 +0530

    Update NQueens

--- standard/backtracking/NQueens/Main.java
+++ standard/backtracking/NQueens/Main.java
@@ -2,44 +2,61 @@ import java.util.*;
 import java.lang.Math;
 
 class Solution {
-    public List<List<String>> solveNQueens(int n) {
-        List<List<String>> ans = new ArrayList<>();
-        List<StringBuilder> temp = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            temp.add(new StringBuilder());
-            for (int j = 0; j < n; j++) {
-                temp.get(i).append('.');
-            }
-        }
-        dfs(ans, temp, n, 0);
-        return ans;
-    }
+    List<List<String>> res = new LinkedList<>();
 
-    public void dfs(List<List<String>> ans, List<StringBuilder> temp, int n, int count) {
-        if (count == n) {
-            ans.add(new ArrayList<>());
-            for (StringBuilder sb : temp) {
-                ans.get(ans.size() - 1).add(sb.toString());
-            }
+    public List<List<String>> solveNQueens(int n) {
+        char[][] board = new char[n][n];
+        for(char[] c : board) {
+            Arrays.fill(c, '.');
+        }
+        backtrack(board, 0);
+        return res;
+    }
+    
+    void backtrack(char[][] board, int row) {
+        if (row == board.length) {
+            res.add(boardToList(board));
             return;
         }
-        for (int i = 0; i < n; i++) {
-            if (isValidPosition(temp, i, count)) {
-                temp.get(i).setCharAt(count, 'Q');
-                dfs(ans, temp, n, count + 1);
-                temp.get(i).setCharAt(count, '.');
+        
+        for (int col = 0; col < board.length; ++col) {
+              if (!isValid(board, row, col)) {
+                  continue;
+             }
+             board[row][col] = 'Q';
+             backtrack(board, row + 1);
+             board[row][col] = '.';
+        }
+        return;
+    }
+    
+    private boolean isValid(char[][] board, int row, int col) {
+        int n = board.length;
+        for (int i = 0; i < n; ++i ) {
+            if(board[i][col] == 'Q') {
+                return false;
             }
         }
-    }
-
-    public boolean isValidPosition(List<StringBuilder> temp, int i, int j) {
-        for (int k = 0; k < temp.size(); k++) {
-            for (int l = 0; l < temp.size(); l++) {
-                int a = Math.abs(k - i), b = Math.abs(l - j);
-                if (temp.get(k).charAt(l) == 'Q' && (a == 0 || b == 0 || a == b))
-                    return false;
+        
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+            if(board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
             }
         }
         return true;
+    }
+    
+    private List<String> boardToList(char[][] board) {
+        List<String> list = new ArrayList<>();
+        for (char[] c : board) {
+            list.add(String.copyValueOf(c));
+        }
+        return list;
     }
 }
