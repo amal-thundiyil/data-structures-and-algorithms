d533a4f5ad00a9dd7a8a66eecab5533f9744d393
Amal Thundiyil <amalthundiyil0@gmail.com>
2022-05-19 20:15:56 +0530

    add: finite automata

--- FiniteAutomata/Main.java
+++ FiniteAutomata/Main.java
@@ -4,13 +4,54 @@ import java.io.*;
 public class Main {
     static Scanner fs;
     static PrintWriter pw;
+    static int CHARS = 256;
 
-    public static void solve() {
-        String txt = fs.nextLine();
-        String ptn = fs.nextLine();
-        int[] lps = new int[ptn.length()];
-        finiteAutomata(ptn, lps);
-        finiteAutomata(txt, ptn, lps);
+    static int getNextState(char[] pat, int M,
+            int state, int x) {
+
+        if (state < M && x == pat[state])
+            return state + 1;
+
+        int ns, i;
+
+        for (ns = state; ns > 0; ns--) {
+            if (pat[ns - 1] == x) {
+                for (i = 0; i < ns - 1; i++)
+                    if (pat[i] != pat[state - ns + 1 + i])
+                        break;
+                if (i == ns - 1)
+                    return ns;
+            }
+        }
+
+        return 0;
+    }
+
+    static void computeTF(char[] pat, int M, int TF[][]) {
+        int state, x;
+        for (state = 0; state <= M; ++state)
+            for (x = 0; x < NO_OF_CHARS; ++x)
+                TF[state][x] = getNextState(pat, M, state, x);
+    }
+
+    static void solve() {
+        char[] txt = fs.nextLine().toCharArray();
+        char[] pat = fs.nextLine().toCharArray();
+
+        int M = pat.length;
+        int N = txt.length;
+
+        int[][] TF = new int[M + 1][NO_OF_CHARS];
+
+        computeTF(pat, M, TF);
+
+        int i, state = 0;
+        for (i = 0; i < N; i++) {
+            state = TF[state][txt[i]];
+            if (state == M)
+                System.out.println("Pattern found "
+                        + "at index " + (i - M + 1));
+        }
     }
 
     public static void main(String[] args) throws Exception {
